1afad90f4729bccff1a1d241d42bb79122a448f2
Ryan <ryanpinas77@gmail.com>
2024-02-11 22:01:12 +0100

    database instaled

--- api/main.go
+++ api/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+    "database/sql"
+    "fmt"
+    "log"
+    "net/http"
+
+    _ "github.com/go-sql-driver/mysql"
+)
+
+var (
+    db *sql.DB
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+    /// BASE DE DONNÉES
+    var err error
+    db, err = sql.Open("mysql", "goteam:root@tcp(localhost:3306)/golang")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    err = db.Ping()
+    if err != nil {
+        log.Fatal(err)
+    }
+    port := 8080
+    fmt.Printf("Server is running on port %d...\n", port)
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
+
